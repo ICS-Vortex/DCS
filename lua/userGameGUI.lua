67a55309af90fd2e64998eb55b41d3a684644494
eekz <s_goretsky@outlook.com>
2015-12-06 15:18:28 +0300

    правки в lua

--- lua/userGameGUI.lua
+++ lua/userGameGUI.lua
@@ -171,8 +171,8 @@ function onGameEvent(eventName,arg1,arg2,arg3,arg4,arg5,arg6,arg7)
 			_killed_target_category = "Ship"
 		end
 		local _kill_score = DCS.getUnitTypeAttribute(arg5, _attributeName_score)
-		--здесь делаем проверку чтобы стороны убийцы и убитого отличались, чтобы избежать записи очков за убийство своих ботов
-		--if _killed_target_side ~= _killer_player_side then
+		--здесь делаем проверку чтобы стороны убийцы и убитого отличались, чтобы избежать записи очков за убийство своих
+		if _killed_target_side ~= _killer_player_side then
 			--если игрока убил другой игрок
 			if _killer_player_name ~= nil and _killed_player_name ~= nil then
 				--отправляем запись об убийстве на сервер статистики
@@ -180,7 +180,7 @@ function onGameEvent(eventName,arg1,arg2,arg3,arg4,arg5,arg6,arg7)
 				--вспомогательная запись в лог. (можно удалить или закомментить)
 				net.log(_killer_target_side..";".._killer_player_name..";killed;".._killer_player_ucid..";".._killed_target_side..";".._killed_player_name..";".._killed_player_ucid..";"..50)
 			--если игрок убил бота
-			elseif _killer_player_name ~= nil and _killed_player_name == nil and _killed_target_side ~= _killer_player_side then
+			elseif _killer_player_name ~= nil and _killed_player_name == nil then
 				--отправляем запись об убийстве на сервер статистики
 				save_stat(_killer_player_name..";killed;".._killer_player_ucid..";".._killed_target_category..";".._killed_target_type..";".._kill_score)
 				--вспомогательная запись в лог. (можно удалить или закомментить)
@@ -220,7 +220,7 @@ function onGameEvent(eventName,arg1,arg2,arg3,arg4,arg5,arg6,arg7)
 				net.log(_killed_player_name..";dead;".._killed_player_ucid)
 			--]]
 			end
-		--end
+		end
     --при самоубийстве игрока, например от собственной бомбы
     elseif eventName == "self_kill" then 
     	local _player_name = net.get_player_info(arg1, 'name')
